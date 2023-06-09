569ceb36655f6b7c1d6d1d80420be9a5f27751d5
Pranto Sheikh <prantosheikh834@gmail.com>
2023-06-09 11:13:42 +0600

    verifyJWT method server impingement

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  // console.log(req.headers);
+
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorization access" });
+  }
+  const token = authorization.split(" ")[1];
+  // console.log(token);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorization access 2" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.efhcwjr.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,6 +51,18 @@ async function run() {
 
     const usersCollections = client.db("SoccerClubDB").collection("users");
 
+    // user related api
+    // get Soccer club all user
+    app.get("/users", verifyJWT, async (req, res) => {
+      const result = await usersCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/instructors", async (req, res) => {
+      const result = await usersCollections.find().toArray();
+      res.send(result);
+    });
+
     // Soccer club single users save
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -37,6 +73,44 @@ async function run() {
         return res.send("User", { message: existionUser });
       }
       const result = await usersCollections.insertOne(user);
+      res.send(result);
+    });
+
+    // jwt assecc token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "6h",
+      });
+      res.send({ token });
+    });
+
+    // admin role curd oparation
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      // create a document that sets the plot of the movie
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollections.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // instructor role curd oparation
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      // create a document that sets the plot of the movie
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollections.updateOne(filter, updateDoc);
       res.send(result);
     });
 
